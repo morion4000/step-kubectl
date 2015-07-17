7cbd552fa634c1ca8175c0e9fa6297dfd1860b2d
Ionut Moraru <morion4000@gmail.com>
2015-07-17 15:13:23 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-main() {
-  true;
-}
+kubectl_command=$WERCKER_KUBECTL_COMMAND
+insecure_verify="false"
 
-main;
+if [ "$WERCKER_KUBECTL_INSECURE_SKIP_TLS_VERIFY" = "true" ] ; then
+  insecure_verify="true"
+fi
+
+if [ -n "$WERCKER_KUBECTL_IMAGE" ] ; then
+  kubectl_command="$kubectl_command --image=$WERCKER_KUBECTL_IMAGE"
+fi
+
+kubectl $kubectl_command --server=$WERCKER_KUBECTL_SERVER --token=$WERCKER_KUBECTL_SERVER --insecure-skip-tls-verify=$insecure_verify
