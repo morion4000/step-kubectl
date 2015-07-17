d1223c00121aaa4b857d2f8a6520c251e3596eac
Ionut Moraru <morion4000@gmail.com>
2015-07-17 16:16:42 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,14 +11,8 @@ if [ -n "$WERCKER_KUBECTL_IMAGE" ] ; then
   kubectl_command="$kubectl_command --image=$WERCKER_KUBECTL_IMAGE"
 fi
 
-pwd
-ls
-
-cd "$WERCKER_STEP_ROOT"
-
-pwd
-ls
+cd $WERCKER_STEP_ROOT
 
 chmod +x kubectl
 
-./kubectl "$kubectl_command --server=$WERCKER_KUBECTL_SERVER --token=$WERCKER_KUBECTL_SERVER --insecure-skip-tls-verify=$insecure_verify"
+./kubectl $kubectl_command --server=$WERCKER_KUBECTL_SERVER --token=$WERCKER_KUBECTL_TOKEN --insecure-skip-tls-verify=$insecure_verify
