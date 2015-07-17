c8dfc853266a0c0a9cafaf4d2aea889e77f0c005
Ionut Moraru <morion4000@gmail.com>
2015-07-17 15:19:01 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,4 +11,4 @@ if [ -n "$WERCKER_KUBECTL_IMAGE" ] ; then
   kubectl_command="$kubectl_command --image=$WERCKER_KUBECTL_IMAGE"
 fi
 
-kubectl $kubectl_command --server=$WERCKER_KUBECTL_SERVER --token=$WERCKER_KUBECTL_SERVER --insecure-skip-tls-verify=$insecure_verify
+kubectl "$kubectl_command --server=$WERCKER_KUBECTL_SERVER --token=$WERCKER_KUBECTL_SERVER --insecure-skip-tls-verify=$insecure_verify"
